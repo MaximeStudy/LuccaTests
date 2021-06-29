6e7a4b9c26a7e9b6021a754827653cd4c0db409d
Maxime Study <mstudy@adents.com>
2021-06-29 14:52:50 +0200

    Manage to load file input and try to parse file

--- LuccaDevises/Program.cs
+++ LuccaDevises/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace LuccaDevises
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please enter a file path.");
+                Console.WriteLine("Usage: LuccaDevises <file path>");
+                return;
+            }
+
+            string filePath = args[0];
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Ok");
+                var inputFile = File.ReadAllText(filePath)
+                                    .Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+                                    .ToList();
+            }
+            else
+            {
+                Console.WriteLine($"File {filePath} does not exists.");
+            }
         }
     }
 }
