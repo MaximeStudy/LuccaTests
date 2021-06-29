044c94eafde2746d0f729f6496dfcfb43a79ad27
Maxime Study <mstudy@adents.com>
2021-06-29 15:03:36 +0200

    add test project

---
+++ LuccaDevises.Tests/ContentValidatorTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LuccaDevises.Tests
+{
+    public class ContentValidatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ LuccaDevises/ContentValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LuccaDevises
+{
+    public class ContentValidator
+    {
+        private readonly List<string> fileContent;
+
+        public ContentValidator(List<string> fileContent)
+        {
+            this.fileContent = fileContent;
+        }
+
+        public bool IsValid()
+        {
+            if (true)
+            {
+            }
+            return true;
+        }
+    }
+}
