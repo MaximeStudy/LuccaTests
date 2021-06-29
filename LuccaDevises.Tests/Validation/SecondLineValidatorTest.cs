b3dfe27328b9a977619b5380cbc687ed6b358ce2
Maxime Study <maxime.study@gmail.com>
2021-06-29 18:28:24 +0200

    add second line validation

--- LuccaDevises.Tests/Validation/SecondLineValidatorTest.cs
+++ LuccaDevises.Tests/Validation/SecondLineValidatorTest.cs
@@ -1,6 +1,34 @@
-﻿namespace LuccaDevises.Tests.Validation
+﻿using LuccaDevises.Validation;
+using Xunit;
+
+namespace LuccaDevises.Tests.Validation
 {
     public class SecondLineValidatorTest
     {
+        [Fact]
+        public void GivenASecondLineWithPositiveInteger_ThenLineIsValid()
+        {
+            //Given
+            IValidator secondLineValidator = new SecondLineValidator("54");
+
+            //When
+            var contentIsValid = secondLineValidator.IsValid();
+
+            //Then
+            Assert.True(contentIsValid);
+        }
+
+        [Fact]
+        public void GivenASecondLineWithoutInteger_ThenLineIsNotValid()
+        {
+            //Given
+            IValidator secondLineValidator = new SecondLineValidator("54;PCD");
+
+            //When
+            var contentIsValid = secondLineValidator.IsValid();
+
+            //Then
+            Assert.False(contentIsValid);
+        }
     }
 }
